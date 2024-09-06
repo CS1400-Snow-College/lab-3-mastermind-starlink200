c2e2ad8f64c922bd46718b19acb299b5440e024a
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2024-09-06 11:05:02 -0600

    created a stop function

--- Program.cs
+++ Program.cs
@@ -4,10 +4,22 @@
 *Date Started: 9/4/2024
 *Date Finished:
 ********************************************************************/
+/*******************************************************************
+*Things left to do in program:
+*If there are repeat letters in an answer make answer invalid
+*or don't count the repeat letter twice
+*
+*Make a winner display
+*
+*Make a play again function
+*
+*Make a guess counter
+********************************************************************/
 Console.Clear();
 Console.WriteLine("Hello! This is a guessing game similar to wordle, except it won't be real word" +
 " instead 4 letters between 'a' and 'g' will be chosen at random, after you guess you will be told how many letters a" +
 "re in the right place and how many letters are in the wrong place");
+Console.WriteLine("You can stop anytime by typing stop");
 //Create a random number generator
 Random rand = new Random();
 //create a list to store the 4 randomly selected characters
@@ -41,36 +53,56 @@ foreach(var letter in randLetters){
 //counters for the number of letters in the correct position and the number of letters correct but in the wrong spot
 int lettersCorrectPosition = 0;
 int lettersIncorrectPosition = 0;
-//boolean to loop program if an invalid answer is given
+//counter for number of guesses
+int guessCounter = 1;
+//boolean to loop program if an invalid answer is given and if user doesn't get correct answer
 bool redoAnswer = true;
 Console.WriteLine("");
-Console.WriteLine("Please guess a sequence of 4 lowercase letters with no spaces.");
 while(redoAnswer){
     redoAnswer = false;
+    Console.Write($"Guess #{guessCounter}: ");
+    Console.WriteLine("Please guess a sequence of 4 lowercase letters with no spaces and no repeats.");
     string usersAnswer = Console.ReadLine();
-    //test to make sure user gives valid answer
-    if(usersAnswer.Length == 4)
+    //if user types stop program will not loop again
+    if(usersAnswer.ToLower().Equals("stop"))
     {
-        //iterates through each letter of the users answer and letter in the list
-        for(int i = 0; i < 4; i++)
-        {
-            foreach(char letter in randLetters)
-            {
-                if(usersAnswer[i].Equals(randLetters[i]))
-                {
-                    lettersCorrectPosition++;
-                }
-                else if(usersAnswer[i].Equals(letter)){
-                    lettersIncorrectPosition++;
-                }
-            }
-        }
-        //since userAnswer is a string each letter takes up the space of 4 char, so we must divide by 4 to get correct number
-        Console.WriteLine($"{lettersCorrectPosition/4} letters in the correct position");
-        Console.WriteLine($"{lettersIncorrectPosition} letters in the incorrect position");
+        redoAnswer = false;
     }
     else{
-        Console.WriteLine("Invalid Answer Please guess again");
-        redoAnswer = true;
+        //test to make sure user gives valid answer
+        if(usersAnswer.Length == 4)
+        {
+            //increment guess counter only if user gives valid answer
+            guessCounter++;
+            //iterates through each letter of the users answer and letter in the list
+            for(int i = 0; i < 4; i++)
+            {
+                foreach(char letter in randLetters)
+                {
+                    if(usersAnswer[i].Equals(randLetters[i]))
+                    {
+                        lettersCorrectPosition++;
+                    }
+                    else if(usersAnswer[i].Equals(letter)){
+                        lettersIncorrectPosition++;
+                    }
+                }
+            }
+            //since userAnswer is a string each letter takes up the space of 4 char, so we must divide by 4 to get correct number
+            Console.WriteLine($"{lettersCorrectPosition/4} letters in the correct position");
+            Console.WriteLine($"{lettersIncorrectPosition} letters in the incorrect position");
+            if(lettersCorrectPosition == 4)
+            {
+                Console.WriteLine($"You guessed it in {guessCounter} tries! The letters were {usersAnswer}!");
+                //redoAnswer = false;
+            }
+            else{
+                redoAnswer = true;
+            }
+        }
+        else{
+            Console.WriteLine("Invalid Answer Please guess again");
+            redoAnswer = true;
+        }
     }
 }
--- obj/Debug/net8.0/lab-3-mastermind-starlink200.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-3-mastermind-starlink200.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-mastermind-starlink200")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2252bf359afafa83fcbf3f82ac885320a7ac9b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ddc1dbb7e8daf7f2fb0d29828bd57ebee6a3c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-mastermind-starlink200")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-mastermind-starlink200")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
